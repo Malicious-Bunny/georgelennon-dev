3f4edb87b1cf6156cf0be257eaf3f8317501008b
Malicious-Bunny <ngohgeorge.mbahnji@ictuniversity.edu.cm>
2024-10-21 21:28:53 +0100

    initial commit

---
+++ src/components/intro.tsx
@@ -0,0 +1,91 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import Link from 'next/link';
+
+import { Button } from '@/components/button';
+import { Icons } from '@/components/icons';
+import { useSectionInView } from '@/hooks/use-section-in-view';
+
+export const Intro = () => {
+  const { ref } = useSectionInView('Home');
+
+  return (
+    <section
+      ref={ref}
+      id="home"
+      className="my-10 flex scroll-mt-96 flex-col items-center gap-4 text-center sm:mt-20"
+    >
+      <motion.a
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+          type: 'tween',
+          duration: 0.2,
+        }}
+        href="https://github.com/Skolaczk/next-starter"
+        className="bg-muted rounded px-3 py-1 text-sm font-medium"
+      >
+        🎉
+        <span className="ml-3">click here to checkout my new blog!</span>
+      </motion.a>
+      <motion.h1
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        className="text-3xl font-bold leading-tight tracking-tighter sm:text-4xl"
+      >
+        Full-stack developer passionate about backend and mobile development
+
+      </motion.h1>
+      <motion.p
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+        className="text-muted-foreground max-w-lg"
+      >
+       Hi, I&apos;m George Lennon, aka malicious-bunny. I&apos;m a full-stack developer specializing in backend ASP.NET and occasional mobile development, passionate about building robust systems and solving complex problems.
+
+      </motion.p>
+      <motion.div
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+        className="flex flex-row gap-2"
+      >
+        <Button asChild size="lg">
+          <Link href="#contact">
+            Get in touch <Icons.arrowRight className="ml-2 size-4" />
+          </Link>
+        </Button>
+        <Button
+          variant="secondary"
+          size="lg"
+          className="hidden sm:flex"
+          asChild
+        >
+          <a href="/michalskolak.pdf" download>
+            Download CV <Icons.download className="ml-2 size-4" />
+          </a>
+        </Button>
+        <Button variant="secondary" size="icon" asChild>
+          <a
+            href="https://www.linkedin.com/in/george-ngoh-aa3411334?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"
+            aria-label="Linkedin"
+            download
+          >
+            <Icons.linkedin className="size-6" />
+          </a>
+        </Button>
+        <Button variant="secondary" size="icon" asChild>
+          <a href="https://github.com/Malicious-Bunny" aria-label="Github">
+            <Icons.github className="size-6" />
+          </a>
+        </Button>
+      </motion.div>
+    </section>
+  );
+};
